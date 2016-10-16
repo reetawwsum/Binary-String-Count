13ab225f92528933dd5f0aa9a3f47736c6981437
Reet Awwsum <reetawwsum@yahoo.com>
2016-10-16 16:29:26 +0530

    Added assertions

--- main.py
+++ main.py
@@ -14,11 +14,19 @@ flags.DEFINE_string('dataset_dir', 'data', 'Directory name to save the dataset')
 flags.DEFINE_string('checkpoint_dir', 'checkpoint', 'Directory name to save the checkpoint')
 flags.DEFINE_string('batch_dataset_type', 'train_dataset', 'Dataset used for generating training batches')
 flags.DEFINE_string('accuracy_dataset_type', 'test_dataset', 'Dataset used for generating accuracy')
-flags.DEFINE_string('model_name', 'lstm-rnn', 'Name of the model')
+flags.DEFINE_string('model_name', 'rnn-lstm', 'Name of the model')
 flags.DEFINE_integer('restore_model', 3110, 'Model to restore to calculate accuracy')
 FLAGS = flags.FLAGS
 
 def main(_):
+	# Validating flags
+	assert FLAGS.num_units > 0, 'Number of units in LSTM layer should be greater than 0'
+	assert FLAGS.num_unrollings > 0, 'Input Sequence length should be greater than 0'
+	assert FLAGS.train_size < 2**FLAGS.num_unrollings, 'The size of training dataset should be less than %d' % 2**FLAGS.num_unrollings
+	assert FLAGS.train_size % FLAGS.batch_size == 0, 'Train size should be divisible by batch size'
+	assert FLAGS.epochs > 0, 'Epochs should be greater than 0'
+	assert FLAGS.checkpoint_step % (FLAGS.train_size / FLAGS.batch_size) == 0, 'Checkpoint step should be an Epoch'
+
 	model = Model(FLAGS)
 
 	if FLAGS.train:
